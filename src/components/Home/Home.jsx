2d9d87015c0aef2b0fdddf9a4ec4f81c73a93a5f
Latios11 <vivekkumar87900@gmail.com>
2024-11-30 21:24:42 +0530

    Responsive home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,26 +5,25 @@ function Home() {
   return (
     <div className='hero'>
         <div className="hero-content">
-           <div className="hero-header">
+          <div className="hero-header">
             I am <span>Vivek kumar</span>,<br/> 
             <span>Front end</span> Developer.
-           </div>
-           <div className="hero-para">
+          </div>
+          <div className="hero-container">
+           <img src="https://images.pexels.com/photos/208147/pexels-photo-208147.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+        </div>
+        <div className="hero-para">
            I’m a passionate Front-End Developer dedicated to 
             creating dynamic and responsive web experiences. 
             With a keen eye for design and a deep understanding of 
             modern technologies, I bring ideas to life through clean, 
-            efficient code and thoughtful user interfaces. Whether you're 
+            efficient code and thoughtful user interfaces. <br/><br/>Whether you're 
             looking for a sleek website, a vibrant application, or a unique 
             solution tailored to your needs, I’m here to help. 
 
             Explore my work and discover how we can collaborate to elevate your digital presence!
-           </div>
-        </div>
-        <div className="hero-img">
-           <img src="https://images.pexels.com/photos/208147/pexels-photo-208147.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        </div>
-              
+          </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Navbar.css"
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+
   return (
     <div className="navbar">
       <div className="navbar-logo">
         Vivek Kumar
       </div>
-      <div className="nav-links">
+      <div className={`menu-icon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <i class="fa-solid fa-bars"></i>
+      </div>
+      <div className={`close-icon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <i class="fa-solid fa-x"></i>
+      </div>
+      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
         <a href="#Home">Home</a>
         <a href="#about">About</a>
         <a href="#services">Projects</a>
